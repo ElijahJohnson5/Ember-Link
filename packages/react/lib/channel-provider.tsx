6bb99aa8a682642b9b3ac1beb70bc5d823add557
Elijah Johnson <elijahjohnson987@gmail.com>
2025-03-25 22:31:42 -0600

    Create the initial react package with basic providers

---
+++ packages/react/lib/channel-provider.tsx
@@ -0,0 +1,120 @@
+import {
+  Channel,
+  type ChannelConfig,
+  type DefaultCustomMessageData,
+  type DefaultPresence,
+  type IStorageProvider
+} from '@ember-link/core';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+  type PropsWithChildren
+} from 'react';
+import { useClient } from './ember-link-provider';
+
+const ChannelContext = createContext<Channel | null>(null);
+
+export const useChannelOrNull = <
+  P extends DefaultPresence,
+  C extends DefaultCustomMessageData
+>(): Channel<P, C> | null => {
+  return useContext(ChannelContext);
+};
+
+export const useChannel = <
+  P extends DefaultPresence,
+  C extends DefaultCustomMessageData
+>(): Channel<P, C> => {
+  const channel = useChannelOrNull();
+
+  if (!channel) {
+    throw new Error('You must call useChannel from inside of a ChannelProvider');
+  }
+
+  return channel;
+};
+
+interface ChannelProviderProps<S extends IStorageProvider, P extends DefaultPresence> {
+  channelName: string;
+  options: ChannelConfig<S, P>['options'];
+}
+
+interface ChannelAndLeave<P extends DefaultPresence, C extends DefaultCustomMessageData> {
+  channel: Channel<P, C>;
+  leave: () => void;
+}
+
+export const ChannelProvider = <
+  S extends IStorageProvider,
+  P extends DefaultPresence,
+  C extends DefaultCustomMessageData
+>(
+  props: PropsWithChildren<ChannelProviderProps<S, P>>
+) => {
+  const client = useClient<P, C>();
+
+  const [cache] = useState(() => new Map<string, ChannelAndLeave<P, C>>());
+
+  const joinChannel = useCallback(
+    (channelName: string, options: ChannelConfig<S, P>['options']) => {
+      const cached = cache.get(channelName);
+
+      if (cached) {
+        return cached;
+      }
+
+      const channelAndLeave = client.joinChannel<S>(channelName, options);
+
+      const oldLeave = channelAndLeave.leave;
+
+      channelAndLeave.leave = () => {
+        oldLeave();
+        cache.delete(channelName);
+      };
+
+      cache.set(channelName, channelAndLeave);
+
+      return channelAndLeave;
+    },
+    [client, cache]
+  );
+
+  return <ChannelProviderInner<S, P, C> {...props} joinChannel={joinChannel} />;
+};
+
+const ChannelProviderInner = <
+  S extends IStorageProvider,
+  P extends DefaultPresence,
+  C extends DefaultCustomMessageData
+>({
+  channelName,
+  options,
+  joinChannel,
+  children
+}: PropsWithChildren<
+  ChannelProviderProps<S, P> & {
+    joinChannel: (
+      channelName: string,
+      options: ChannelConfig<S, P>['options']
+    ) => ChannelAndLeave<P, C>;
+  }
+>) => {
+  const [{ channel }, setChannelLeavePair] = useState(() => {
+    return joinChannel(channelName, options);
+  });
+
+  useEffect(() => {
+    const channelLeavePair = joinChannel(channelName, options);
+
+    setChannelLeavePair(channelLeavePair);
+
+    return () => {
+      channelLeavePair.leave();
+    };
+  }, [channelName, joinChannel]);
+
+  return <ChannelContext.Provider value={channel as Channel}>{children}</ChannelContext.Provider>;
+};
---
+++ packages/react/lib/ember-link-provider.tsx
@@ -0,0 +1,52 @@
+import {
+  createClient,
+  type CreateClientOptions,
+  type DefaultCustomMessageData,
+  type DefaultPresence,
+  type EmberClient
+} from '@ember-link/core';
+import { createContext, useContext, useMemo, type PropsWithChildren } from 'react';
+
+const ClientContext = createContext<EmberClient | null>(null);
+
+export const useClientOrNull = <
+  P extends DefaultPresence,
+  C extends DefaultCustomMessageData
+>(): EmberClient<P, C> | null => {
+  return useContext(ClientContext);
+};
+
+export const useClient = <
+  P extends DefaultPresence,
+  C extends DefaultCustomMessageData
+>(): EmberClient<P, C> => {
+  const client = useClientOrNull<P, C>();
+
+  if (!client) {
+    throw new Error('You must call useClient from inside of a EmberLinkProvider');
+  }
+
+  return client;
+};
+
+const useEnsureNoEmberLinkProvider = () => {
+  const client = useClientOrNull();
+
+  if (client) {
+    throw new Error('You cannot nest multiple EmberLinkProviders in the same tree');
+  }
+};
+
+export const EmberLinkProvider = <
+  P extends Record<string, unknown> = DefaultPresence,
+  C extends Record<string, unknown> = DefaultCustomMessageData
+>({
+  children,
+  ...options
+}: PropsWithChildren<CreateClientOptions>) => {
+  useEnsureNoEmberLinkProvider();
+
+  const client = useMemo(() => createClient<P, C>(options), []);
+
+  return <ClientContext.Provider value={client}>{children}</ClientContext.Provider>;
+};
