a8df9a7cf255c545f8b059e2b8ce7787bfa32705
Elijah Johnson <elijahjohnson987@gmail.com>
2025-03-26 19:26:14 -0600

    Update svelte package to have correct types

--- packages/svelte/src/lib/ClientProvider.svelte
+++ packages/svelte/src/lib/ClientProvider.svelte
@@ -1,25 +1,37 @@
 <script lang="ts" module>
-  export const name = "EMBER_LINK_CLIENT";
+	export const name = 'EMBER_LINK_CLIENT';
 
-  export function getClientContext<P extends Record<string, unknown> = DefaultPresence>(): EmberClient<P> {
-    if (!hasContext(name)) {
-      throw new Error('Get client context must be called inside of a client provider')
-    }
+	export function getClientContext<
+		P extends Record<string, unknown> = DefaultPresence,
+		C extends Record<string, unknown> = DefaultCustomMessageData
+	>(): EmberClient<P, C> {
+		if (!hasContext(name)) {
+			throw new Error('Get client context must be called inside of a client provider');
+		}
 
-    return getContext(name);
-  }
+		return getContext(name);
+	}
 </script>
 
-<script lang="ts" generics="P extends Record<string, unknown> = DefaultPresence">
-	import { getContext, hasContext, setContext, type Snippet } from "svelte";
-  import { createClient, type CreateClientOptions, type DefaultPresence, type EmberClient } from "@ember-link/core";
+<script
+	lang="ts"
+	generics="P extends Record<string, unknown> = DefaultPresence, C extends Record<string, unknown> = DefaultCustomMessageData"
+>
+	import { getContext, hasContext, setContext, type Snippet } from 'svelte';
+	import {
+		createClient,
+		type CreateClientOptions,
+		type DefaultCustomMessageData,
+		type DefaultPresence,
+		type EmberClient
+	} from '@ember-link/core';
 
-  const { clientOptions, children }: { clientOptions: CreateClientOptions, children: Snippet<[]> } = $props();
+	const { clientOptions, children }: { clientOptions: CreateClientOptions; children: Snippet<[]> } =
+		$props();
 
-  const client = createClient<P>(clientOptions)
+	const client = createClient<P, C>(clientOptions);
 
-  setContext(name, client);
+	setContext(name, client);
 </script>
-
 
 {@render children?.()}
